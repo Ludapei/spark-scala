98b68ae8ee42520fcaa4b22a6372fc09e446ba63
Ludapei <1304574071@qq.com>
2019-06-18 01:03:20 +0800

    upload

--- src/main/java/com/grg/java/JavaSpark2.java
+++ src/main/java/com/grg/java/JavaSpark2.java
@@ -4,18 +4,27 @@ import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
+import org.apache.spark.sql.SQLContext;
 import org.apache.spark.sql.SparkSession;
 
+/**
+ *   DataFrame/RDD/DataSet的异同
+ *
+ *   DataFrame~= Dataset<Row>
+ *
+ *   https://www.jianshu.com/p/b9a7a650a781
+ */
 public class JavaSpark2 {
     public static void main(String[] args) {
 
         SparkConf conf = new SparkConf().setAppName("TxtToParquet").setMaster("local");
-        SparkSession spark = SparkSession.builder().config(conf).getOrCreate();
-//        reflectTransform(spark);//Java反射
-//        dynamicTransform(spark);//动态转换
+        SparkSession sc = SparkSession.builder().config(conf).getOrCreate();
+        SQLContext sqlContext = new org.apache.spark.sql.SQLContext(sc);
 
-        JavaRDD<String> source = spark.read().textFile("C:\\\\MyApplication\\\\Idea\\\\Project_bigdata\\\\sparkdemo3\\\\data3.csv").javaRDD();
+        //修改路径
+        JavaRDD<String> source = sc.read().textFile("C:\\\\MyApplication\\\\Idea\\\\Project_bigdata\\\\sparkdemo3\\\\data3.csv").javaRDD();
 
+        // 将数据与类名对应
         JavaRDD<Student> rowRDD = source.map(line -> {
             String parts[] = line.split(",");
             Student stu = new Student();
@@ -25,10 +34,31 @@ public class JavaSpark2 {
             return stu;
         });
 
-        Dataset<Row> df = spark.createDataFrame(rowRDD, Student.class);
+
+        Dataset<Row> df = sc.createDataFrame(rowRDD, Student.class);
 
         df.select("sid", "sname", "sage");
+
+        df.select("sname", "sage");
         df.show();
+
+
+        System.out.println("use sparkSql");
+        df.registerTempTable("t_person");  //赋予表名
+
+
+        System.out.println("df2");
+        Dataset<Row> df2=sqlContext.sql("select * from t_person order by sid  limit 2");
+        df2.show();
+
+
+
+
+        System.out.println("df3");
+        df.createOrReplaceTempView("tmp");
+        Dataset<Row> df3=sqlContext.sql("select sage,sname from tmp where sage>14");
+        df3.show();
+
 
     }
 }
