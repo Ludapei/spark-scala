ec7715b314d3cb7586e50b6bef8bab37d7ef3e9a
Ludapei <1304574071@qq.com>
2019-06-27 23:48:36 +0800

    group by

--- src/main/java/com/grg/java/JavaSpark3.java
+++ src/main/java/com/grg/java/JavaSpark3.java
@@ -2,10 +2,14 @@ package com.grg.java;
 
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.function.VoidFunction;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SQLContext;
 import org.apache.spark.sql.SparkSession;
+
+import java.util.HashMap;
+import java.util.Iterator;
 
 /**
  *   DataFrame/RDD/DataSet的异同
@@ -22,32 +26,38 @@ public class JavaSpark3 {
         SQLContext sqlContext = new SQLContext(sc);
 
         //修改路径
-        JavaRDD<String> source = sc.read().textFile("C:\\MyApplication\\Idea\\Project_bigdata\\sparkdemo3\\data4.csv").javaRDD();
+        JavaRDD<String> source = sc.read().textFile("C:\\MyApplication\\Idea\\Project_bigdata\\sparkdemo3\\data5.csv").javaRDD();
 
-        // 将数据与类名对应
-        JavaRDD<Data> rowRDD = source.map(line -> {
-            String parts[] = line.split(",");
-            Data data = new Data();
-            data.setA(Integer.valueOf(parts[0]));
-            data.setB(Integer.valueOf(parts[1]));
-            data.setC(Integer.valueOf(parts[2]));
-            return data;
+        source.foreachPartition(new VoidFunction<Iterator<String>>() {
+        @Override
+        public void call(Iterator<String> iter) throws Exception {
+            HashMap<String, String> hm = new HashMap<>();
+            while(iter.hasNext()) {
+                String[] next=iter.next().split(",");
+                if (hm.get(next[0].trim()) != null) {
+                    hm.put(next[0], hm.get(next[0]) + "," + next[1]);
+                } else {
+                    hm.put(next[0], next[1]);
+                }
+            }
+            for (String key : hm.keySet()) {
+                System.out.println(key + ":"+hm.get(key));
+            }
+         }
         });
 
 
-        Dataset<Row> df = sc.createDataFrame(rowRDD, Data.class);
-
-
-
-        System.out.println("use sparkSql");
-        df.registerTempTable("data");  //赋予表名
-
-
-        System.out.println("df2");
-        Dataset<Row> df2=sqlContext.sql("select a,b,c,a+b*2+c*3 d from data ");
-        df2.show();
-
-
+//        Dataset<Row> df = sc.createDataFrame(rowRDD, Data.class);
+//
+//
+//
+//        System.out.println("use sparkSql");
+//        df.registerTempTable("data");  //赋予表名
+//
+//
+//        System.out.println("df2");
+//        Dataset<Row> df2=sqlContext.sql("select a,b,c,a+b*2+c*3 d from data ");
+//        df2.show();
 
 
 
